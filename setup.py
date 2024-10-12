c4690d0505e4e013c421af89469fcf7f9bf8f6fe
Bondarchuk Gleb <72991231+BondarchukGleb42@users.noreply.github.com>
2024-10-12 12:37:03 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
-    name="streamlit-audiorec",
-    version="0.1.3",
+    name="streamlit-audiorec-ru",
+    version="0.1.4",
     author="Stefan Rummer",
-    author_email="",
+    author_email="djgleban1147@gmail.com",
     description="Record audio from the user's microphone in apps that are deployed to the web. (via Browser Media-API) [GitHub ☆ 160+: steamlit-audio-recorder]",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/stefanrmmr/streamlit-audio-recorder",
+    url="https://github.com/BondarchukGleb42/streamlit-audio-recorder",
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[],
